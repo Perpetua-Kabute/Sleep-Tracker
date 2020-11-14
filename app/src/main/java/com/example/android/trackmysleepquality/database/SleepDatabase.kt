f5eff0591ea704deb9452860fca907c2fdf07ddb
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-14 15:36:02 +0300

    feat: Create room database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,51 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version= 1, exportSchema = false )
+abstract class SleepDatabase: RoomDatabase(){
+    //declare an abstract value of sleepDatabaseDao
+    abstract var sleepDatabaseDao: SleepDatabaseDao
+
+    //declare a companion object
+    companion object{
+
+        //declare a @volatile INSTANCE variable
+        //INSTANCE will keep a reference to the database meaning we don't repeatedly open connections to the db which is expensive
+        //changes made to one thread of INSTANCE are visible to all threads
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        //define getinstance method with a synchronised block
+        fun getInstance(context: Context): SleepDatabase{
+            //synchronised ensures that when multiple threads ask for access only one thread can enter the block of code ensuring the db gets initialized once
+            synchronized(this){
+                var instance = INSTANCE
+
+                //check if database already exists
+                //if not use Room.DatabaseBuilder to create it
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
+    }
+
+
+
+
+}
